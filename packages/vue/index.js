bf30b6ad78cdf78f3d8d601ee759df1d4d056f44
袁泽庭 <chongyun@boomlink.com>
2022-07-06 14:49:34 +0800

    add config files

---
+++ packages/vue/index.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: ['plugin:vue/recommended', '@eriksyuan/eslint-config-ts'],
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      rules: {
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+  ],
+  rules: {
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-v-html': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/v-on-event-hyphenation': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+  },
+}
